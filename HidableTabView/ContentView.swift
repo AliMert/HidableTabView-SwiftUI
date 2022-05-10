5bb9a67e6b6d0999234e30f9c4ba09ce8a9e73c0
Ali Mert Özhayta <mozhayta@hotmail.com>
2022-05-10 19:57:21 +0300

    Initial Commit

---
+++ HidableTabView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HidableTabView
+//
+//  Created by Ali Mert Özhayta on 10.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HidableTabView/HidableTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HidableTabViewApp.swift
+//  HidableTabView
+//
+//  Created by Ali Mert Özhayta on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct HidableTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
